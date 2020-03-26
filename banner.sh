9d131286fda41dad36306804c0b5f8898765e1d5
basu021 <57030150+basu021@users.noreply.github.com>
2020-03-26 12:25:30 +0530

    Add files via upload

---
+++ banner.sh
@@ -0,0 +1,12 @@
+ echo "  _____    ____ "|lolcat
+ echo " |  __ \  |  _ \ "
+ echo " | |  | | | |_) |  ____  ___  "|lolcat
+ echo " | |  | | |  _ <  / _  |/ __|| | | |"
+ echo " | |__| | | |_) || (_| |\__ \| |_| |" |lolcat
+ echo " |_____/  |____/  \__,_||___/ \__,_|"|lolcat
+ echo "                                    "     |lolcat
+ echo " __________________________________________________________________"|lolcat       
+ echo -e "\e[38;5;82m               ╔╦╗┌─┐┌─┐┬  ┌─┐    © 2020 Instagram- \e[38;5;198m@basudevrout2001"|lolcat 
+ echo "                ║ │ ││ ││  └─┐  Creted by~~ Basudev "|lolcat
+ echo "                ╩ └─┘└─┘┴─┘└─┘   ------------------- "  |lolcat                                      
+
