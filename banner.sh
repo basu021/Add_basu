d5f59628a9fb12de98bc57093df60f61a404f215
basu021 <57030150+basu021@users.noreply.github.com>
2020-04-16 19:53:42 +0530

    Add files via upload

---
+++ banner.sh
@@ -0,0 +1,23 @@
+ sleep 1s
+ clear
+ echo " _________________  # # # # # # # # # # # # # # # # # # # #"
+ echo "| ╔╦╗ ╔╦╗ ╔═╗ ╔═╗ | # A simple Addition, Subtraction      #"|lolcat
+ echo "|  ║║ ║║║ ╠═╣ ╚═╗ | # Multiplication and Division Program #"|lolcat
+ echo "| ═╩╝ ╩ ╩ ╩ ╩ ╚═╝ | # using C.                            #"|lolcat
+ echo "|*****************| # # # # # # # # # # # # # # # # # # # #"
+ echo " ================="|lolcat 
+ echo " ------------------   +++++++++++++++++++++++++++++++++++++"|lolcat
+ echo "|  ╔╦╗┌─┐┌─┐┬  ┌─┐ |  + © 2020 Instagram- @basudevrout2001+ " |lolcat 
+ echo "|   ║ │ ││ ││  └─┐ |  + Creted by~~ Basudev   [v- 1.4]    +"|lolcat
+ echo "|   ╩ └─┘└─┘┴─┘└─┘ |  +++++++++++++++++++++++++++++++++++++"  |lolcat
+ echo " ------------------"
+ echo -e "\e[35;5;88m   ╔══════════════════════════════════════════════════╗\e[0m"
+ echo -e "\e[35;5;88m   ║\e[0m\e[1m# Current version:- Bug fixed. New options.       \e[35;5;88m║\e[0m"
+ echo -e "\e[35;5;88m   ║\e[0m\e[1m#                :- New banner and color.         \e[35;5;88m║\e[0m"
+ echo -e "\e[35;5;88m   ║\e[0m\e[1m#                                                 \e[35;5;88m║\e[0m"
+ echo -e "\e[35;5;88m   ║\e[0m\e[1m# Upcomming updates:- Calculate  Square and cube  \e[35;5;88m║\e[0m"
+ echo -e "\e[35;5;88m   ║\e[0m\e[1m#                     of a Number.                \e[35;5;88m║\e[0m"
+ echo -e "\e[35;5;88m   ║\e[0m\e[1m# Sqrt of a number.                               \e[35;5;88m║\e[0m"
+ echo -e "\e[35;5;88m   ║\e[0m\e[1m# THANKS FOR YOUR SUPPORT GUYS...                \e[35;5;88m ║\e[0m"
+ echo -e "\e[35;5;88m   ║                                                  ║\e[0m"
+ echo -e "\e[35;5;88m   ╚══════════════════════════════════════════════════╝\e[0m"
