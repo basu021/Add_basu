f740c9d09295299b32e4bbad3982140e7a596688
basu021 <57030150+basu021@users.noreply.github.com>
2020-04-21 18:44:31 +0530

    Add files via upload

---
+++ r-install.sh
@@ -0,0 +1,21 @@
+#!bin/bash
+
+clear
+pkg upgrade -y
+apt install figlet -y
+figlet -f small Installing...
+sleep 1s
+figlet -f small please wait
+apt install toilet -y
+apt install cowsay -y
+apt install nano -y
+gem install lolcat
+pkg install clang -y
+
+echo -e "\e[35mCompleted"
+figlet -f big Done !!! |lolcat
+echo -e "\e[93mNow run ⟩bash run.sh\e[0m...!!!"
+echo ''
+echo -e "\e[1m\e[32m Devlope4d by :\e[33m Instagram- @basudevrout2001"
+echo ''
+echo ''
