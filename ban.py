e27f2aa0900e1cebd35468e6f86ca3d9bf190c8d
basu021 <57030150+basu021@users.noreply.github.com>
2020-04-21 16:47:41 +0530

    Add files via upload

---
+++ ban.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import time
+#colors
+g='\033[1;32m'
+p='\033[1;35m'
+cyan='\033[1;36m'
+green='\033[1;32m'
+red='\033[1;31m'
+yellow='\033[1;33m'
+blue='\033[1;34m'
+purple='\033[1;35m'
+reset='\033[0m'
+y='\033[1;33m'
+time.sleep(0.01)
+print (cyan  +'          _____________________________________________    ')
+time.sleep(0.01)
+print (cyan +'         !\___________________________________________/!\ ')
+time.sleep(0.01)
+print (cyan +'         !!                                           !! \ ')
+time.sleep(0.01)
+print (cyan +'         !!           Updating { '+red+'Add_tool'+cyan+' }           !!  \ ')
+time.sleep(0.01)
+print (cyan +'         !!                                           !!  ! ')
+time.sleep(0.01)
+print (cyan +'         !!                                           !!  ! ')
+time.sleep(0.01)
+print (cyan +'         !!  version :  { '+red+'1.6'+cyan+' }                       !!  ! ')
+time.sleep(0.01)
+print (cyan +'         !!                                           !!  ! ')
+time.sleep(0.01)
+print (cyan +'         !!                                           !!  ! ')
+time.sleep(0.01)
+print (cyan +'         !!  programmer :  { '+red+'Basudev'+cyan+' }                !!  ! ')
+time.sleep(0.01)
+print (cyan +'         !!                                           !!  ! ')
+time.sleep(0.01)
+print (cyan +'         !!                                           !!  ! ')
+time.sleep(0.01)
+print (cyan +'         !!  github :  { '+red+'github.com/basu021'+cyan+' }         !!  ! ')
+time.sleep(0.01)
+print (cyan +'         !!                                           !!  ! ')
+time.sleep(0.01)
+print (cyan +'         !!                                           !!  / ')
+time.sleep(0.01)
+print (cyan +'         !!___________________________________________!! / ')
+time.sleep(0.01)
+print (cyan +'         !/___________________________________________\!/ ')
+time.sleep(0.01)
+print (cyan +'            __\_____________________________________/__/!_ ')
+time.sleep(0.01)
+print (cyan +'           !_________________________________________!/    ')
+time.sleep(0.01)
+print (cyan +'              _____________________________________    ')
+time.sleep(0.01)
+print (cyan +'            /oooo  oooo  oooo  oooo  oooo  oooo /!      ')
+time.sleep(0.01)
+print (cyan +'           /ooooooooooooooooooooooooooooooooooo/ / ')
+time.sleep(0.01)
+print (cyan +'          /ooooooooooooooooooooooooooooooooooo/ / ')
+time.sleep(0.01)
+print (cyan +'         /ooooooooooooooooooooooooooooooooooo/ / ')
+time.sleep(0.01)
+print (cyan +'        /C=_________________________________/_/ ')
+time.sleep(0.01)
---
+++ update.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+
+
+echo ''
+echo ''
+echo ''
+
+figlet -f small Please wait...|lolcat
+
+sleep 1s
+
+figlet -f small Wait |lolcat
+
+
+clear
+sleep 0.6s
+echo ''
+
+python2 ban.py
+sleep 0.5s
+echo ''
+echo ''
+echo ''
+
+echo please wait !!!
+cd
+
+rm -rf Add_basu
+
+git clone https://github.com/basu021/Add_basu
+
+sleep 0.3s
+echo ''
+echo ''
+
+echo "█...1% completed"
+sleep 0.2s
+echo ''
+echo "█████▓...7% completed"
+sleep 0.3s
+echo " "
+echo "██████████...18% completed"
+
+sleep 0.3s
+echo " "
+echo "████████████████...46% completed"
+sleep 0.5s
+echo " "
+echo "████████████████████████...78% completed"
+sleep 0.3s
+echo " "
+echo "████████████████████████████...83% completed"
+sleep 0.4s
+echo " "
+echo "█████████████████████████████████...93% completed"
+sleep 0.9s
+echo " "
+echo "██████████████████████████████████████...100% completed"
+sleep 0.7s
+echo " "
+echo -e "\e[38;5;120m Done...!\e[0m"
+
+sleep 3s
+echo " "
